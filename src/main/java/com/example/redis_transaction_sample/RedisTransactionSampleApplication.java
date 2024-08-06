136327557083181417a710b144d70e77230ac793
SPAMMAYO <nucon84@gmail.com>
2024-08-06 10:24:31 +0900

    Redis App transaction 구현을 위한 기초 실습입니다

---
+++ src/main/java/com/example/redis_transaction_sample/RedisTransactionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis_transaction_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTransactionSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisTransactionSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redis_transaction_sample/RedisTransactionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis_transaction_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTransactionSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
